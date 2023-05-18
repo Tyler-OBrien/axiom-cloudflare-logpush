9859b85322ff7a82f3c3b57f560223977b039d60
Tyler-OBrien <94197210+Tyler-OBrien@users.noreply.github.com>
2023-05-17 21:03:00 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+Sample of using Axiom with Worker Trace Events Logpush
+
+All Credit to https://gist.github.com/stefandanaita/88c4d8b187400d5b07524cd0a12843b2 for most of the code.
+
+# How To Use
+
+## Clone Repo
+
+
+## Add Authorization Secret
+ wrangler secret put AuthSecret
+ > This is just Axiom's API Token
+
+
+ ## Add Logpush Secret
+ wrangler secret put LogPushSecret
+ > The value can be whatever you want. It's the secret you will use in your logpush config to make sure only logpush can use the endpoint. Make it reasonably secure.
+
+## Deploy
+wrangler publish
+
+## Create LogPush Job
+Example:
+```
+curl -s -X POST 'https://api.cloudflare.com/client/v4/accounts/{AccountID}/logpush/jobs' -X POST -d '
+{
+  "name": "logpush-to-worker",
+  "destination_conf": "https://{YourWorkerName}.{YourSubdomain}.workers.dev/cloudflare?header_Authorization={LogPushSecret}",
+  "dataset": "workers_trace_events",
+  "enabled": true
+}' -H "X-Auth-Email: ....." -H "X-Auth-Key: ...."
+```
+
+Insert your Account ID (find in Workers Tab on Right side), your worker domain, your logpush secret, and CF API Auth Details (either Global API Key or API Token)
+The dataset is the path.
+
+
+CF will send a test event. If it works, then congrats you're done. Check in Axiom for your events and take a nap.
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+// All Credit to https://gist.github.com/stefandanaita/88c4d8b187400d5b07524cd0a12843b2
+// SPDX-License-Identifier: MIT-0
+
+export interface Env {
+  AuthSecret: string;
+  LogPushSecret: string;
+  Dataset: string;
+}
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    var url = new URL(request.url);
+    if (request.headers.get("Authorization") !== env.LogPushSecret) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    if (!request.body) {
+      return new Response("No Body", { status: 500 });
+    }
+    const events = request.body
+      .pipeThrough(new DecompressionStream("gzip"))
+      .pipeThrough(new TextDecoderStream())
+      .pipeThrough(readlineStream());
+
+    let eventsToPush: string[] = [];
+    for await (const event of streamAsyncIterator(events)) {
+      // Do stuff with the event
+      eventsToPush.push(JSON.parse(event));
+    }
+    let json = JSON.stringify(eventsToPush);
+    let dataset = url.pathname.substring(1);
+    var response = await fetch(`https://api.axiom.co/v1/datasets/${dataset}/ingest`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.AuthSecret}`,
+      },
+      body: json,
+    });
+    if (response.ok == false) {
+      return new Response(`Axiom responded with: ${await response.text()}`, {
+        status: 500,
+      });
+    }
+
+    return new Response("Nom nom!", { status: 202 });
+  },
+};
+
+async function* streamAsyncIterator(stream: ReadableStream) {
+  // Get a lock on the stream
+  const reader = stream.getReader();
+
+  try {
+    while (true) {
+      // Read from the stream
+      const { done, value } = await reader.read();
+
+      // Exit if we're done
+      if (done) {
+        return;
+      }
+
+      // Else yield the chunk
+      yield value;
+    }
+  } finally {
+    reader.releaseLock();
+  }
+}
+
+interface ReadlineTransformerOptions {
+  skipEmpty: boolean;
+}
+
+const defaultOptions: ReadlineTransformerOptions = {
+  skipEmpty: true,
+};
+
+export class ReadlineTransformer implements Transformer {
+  options: ReadlineTransformerOptions;
+  lastString: string;
+  separator: RegExp;
+
+  public constructor(options?: ReadlineTransformerOptions) {
+    this.options = { ...defaultOptions, ...options };
+    this.lastString = "";
+    this.separator = /[\r\n]+/;
+  }
+
+  public transform(
+    chunk: string,
+    controller: TransformStreamDefaultController<string>
+  ) {
+    // prepend with previous string (empty if none)
+    const str = `${this.lastString}${chunk}`;
+    // Extract lines from chunk
+    const lines = str.split(this.separator);
+    // Save last line as it might be incomplete
+    this.lastString = (lines.pop() || "").trim();
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const line of lines) {
+      const d = this.options.skipEmpty ? line.trim() : line;
+      if (d.length > 0) controller.enqueue(d);
+    }
+  }
+
+  public flush(controller: TransformStreamDefaultController<string>) {
+    if (this.lastString.length > 0) controller.enqueue(this.lastString);
+  }
+}
+
+export const readlineStream = () =>
+  new TransformStream(new ReadlineTransformer());
